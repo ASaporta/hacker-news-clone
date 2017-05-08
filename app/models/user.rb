6828c4706148e2ab26ce51a2374b0a7039706f4a
Adriel Saporta <email": null>
2017-05-08 16:39:30 -0400

    Add bcrypt and has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: "submitter_id", class_name: "Post", dependent: :destroy
   has_many :comments, foreign_key: "commenter_id", dependent: :destroy
   validates :username, presence: true, uniqueness: true
+
+  has_secure_password
 end
